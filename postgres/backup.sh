7d522fefafdb272f16d987db5456d44ed3ab15cc
Jansen Fuller <jansendfuller@mailfence.com>
2024-12-11 19:59:07 -0700

    Fixed backup script

--- postgres/backup.sh
+++ postgres/backup.sh
@@ -2,20 +2,19 @@
 
 cd /mnt/storage/postgres
 
-docker exec -t postgres-pgvecto-16-1 pg_dumpall -c -U postgres > ./postgres/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
-
+docker exec -t postgres-pgvecto-16-1 pg_dumpall -c -U postgres > ./dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
 LOCATION=/mnt/storage/postgres
 FILECOUNT=0
 
 FILES=$(find $LOCATION -type f)
 
-
 for f in $FILES
 	do
    		FILECOUNT=$[$FILECOUNT+1]
 done
 
-if [ $FILECOUNT >= 11]
+if [ $FILECOUNT -gt 10 ]
+then
 	stat --printf='%Y %n\0' ./* | sort -zn | sed -z 's/[^ ]\{1,\} //;q' | xargs -r0 rm
 fi

800f7100d8027dd9844ad4e893e5ad0505225464
Jansen Fuller <jansendfuller@mailfence.com>
2024-12-11 19:51:04 -0700

    Switched over to seperate Postgres

---
+++ postgres/backup.sh
@@ -0,0 +1,21 @@
+# Backup postgres installation
+
+cd /mnt/storage/postgres
+
+docker exec -t postgres-pgvecto-16-1 pg_dumpall -c -U postgres > ./postgres/dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+
+
+LOCATION=/mnt/storage/postgres
+FILECOUNT=0
+
+FILES=$(find $LOCATION -type f)
+
+
+for f in $FILES
+	do
+   		FILECOUNT=$[$FILECOUNT+1]
+done
+
+if [ $FILECOUNT >= 11]
+	stat --printf='%Y %n\0' ./* | sort -zn | sed -z 's/[^ ]\{1,\} //;q' | xargs -r0 rm
+fi

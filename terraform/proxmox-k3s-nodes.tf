dc45f5e76f13b919cab8e09d800c94334492f50b
Jansen Fuller <jansendfuller@mailfence.com>
2025-09-28 14:51:09 -0600

    Working Terraform

--- terraform/proxmox-k3s-nodes.tf
+++ terraform/proxmox-k3s-nodes.tf
@@ -1,89 +1,23 @@
-terraform {
-  required_providers {
-    proxmox = {
-      source  = "Telmate/proxmox"
-      version = ">= 2.9.9"
-    }
-  }
-}
-
-provider "proxmox" {
-  pm_api_url      = var.proxmox_api_url
-  pm_user         = var.proxmox_user
-  pm_password     = var.proxmox_password
-  pm_tls_insecure = true
-}
-
-variable "proxmox_api_url" {
-  description = "Proxmox API endpoint, e.g. https://proxmox.example.local:8006/api2/json"
-  type        = string
-}
-
-variable "proxmox_node_id" {
-  description = "ID of worker node"
-  type        = string
-}
-
-variable "proxmox_user" {
-  description = "Proxmox username with API privileges, e.g. root@pam or svc-terraform@pve"
-  type        = string
-  default     = "terraform"
-}
-
-variable "proxmox_password" {
-  description = "Proxmox password or API token secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "proxmox_node" {
-  description = "The Proxmox cluster node name to provision the VMs on"
-  type        = string
-}
-
-variable "template_vm_id" {
-  description = "VMID of an existing Alpine cloud-init template in Proxmox"
-  type        = number
-}
-
-variable "vm_storage" {
-  description = "Storage identifier for VM disk, e.g. local-lvm"
-  type        = string
-  default     = "local-lvm"
-}
-
-variable "vm_bridge" {
-  description = "Network bridge to attach the VMs to, e.g. vmbr0"
-  type        = string
-  default     = "vmbr0"
-}
-
-variable "ssh_public_key" {
-  description = "SSH public key to install in the VMs"
-  type        = string
-}
-
 # Master VM (2 core, 4 GB)
 resource "proxmox_vm_qemu" "master" {
   name        = "alpine-k8s-master-${var.proxmox_node_id}"
-  target_node = var.proxmox_node
+  target_node = "worker-${var.proxmox_node_id}"
 
   clone      = var.template_vm_id
   full_clone = true
 
-  cores   = 2
-  sockets = 1
+  cpu { cores   = 2 }
   memory  = 4096
 
   scsihw = "virtio-scsi-pci"
   disk {
+    slot         = "scsi0"
     size         = "24G"
-    type         = "scsi"
     storage      = var.vm_storage
-    storage_type = "lvm"
   }
 
   network {
+    id = var.proxmox_node_id
     model  = "virtio"
     bridge = var.vm_bridge
   }
@@ -91,31 +25,36 @@ resource "proxmox_vm_qemu" "master" {
   agent     = 1
   ciuser    = "root"
   sshkeys  = var.ssh_public_key
-  ipconfig0 = "ip=dhcp"
+
+  # (Optional) IP Address and Gateway
+  ipconfig0 = "ip=10.12.41.1${var.proxmox_node_id}0/16,gw=10.12.10.1"
+
+  # (Optional) Name servers
+  nameserver = "10.12.10.1"
 }
+
 
 # Worker VMs (each 1 core, 2 GB)
 resource "proxmox_vm_qemu" "worker" {
   count       = 2
   name        = "alpine-k8s-worker-${var.proxmox_node_id}0${count.index + 1}"
-  target_node = var.proxmox_node
+  target_node = "worker-${var.proxmox_node_id}"
 
   clone      = var.template_vm_id
   full_clone = true
 
-  cores   = 1
-  sockets = 1
+  cpu { cores   = 1 }
   memory  = 2048
 
   scsihw = "virtio-scsi-pci"
   disk {
+    slot         = "scsi0"
     size         = "16G"
-    type         = "scsi"
     storage      = var.vm_storage
-    storage_type = "lvm"
   }
 
   network {
+    id = count.index + 1 + (10 * var.proxmox_node_id)
     model  = "virtio"
     bridge = var.vm_bridge
   }
@@ -123,7 +62,11 @@ resource "proxmox_vm_qemu" "worker" {
   agent     = 1
   ciuser    = "root"
   sshkeys  = var.ssh_public_key
-  ipconfig0 = "ip=dhcp"
+  # (Optional) IP Address and Gateway
+  ipconfig0 = "ip=10.12.41.${var.proxmox_node_id}${count.index + 1}/16,gw=10.12.10.1"
+
+  # (Optional) Name servers
+  nameserver = "10.12.10.1"
 }
 
 output "master_vmid" {
---
+++ terraform/proxmox-worker-provider.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.2-rc04"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url          = "https://worker-${var.proxmox_node_id}.ff.home:8006/api2/json"
+  pm_api_token_id     = "terraform@pve!tat"
+  pm_api_token_secret = var.proxmox_password
+  pm_tls_insecure     = true
+}
+
+variable "proxmox_node_id" {
+  description = "ID of worker node"
+  type        = number
+}
+
+variable "proxmox_password" {
+  description = "Proxmox password or API token secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "template_vm_id" {
+  description = "VMID of an existing Alpine cloud-init template in Proxmox"
+  type        = number
+}
+
+variable "vm_storage" {
+  description = "Storage identifier for VM disk, e.g. local-lvm"
+  type        = string
+  default     = "local-lvm"
+}
+
+variable "vm_bridge" {
+  description = "Network bridge to attach the VMs to, e.g. vmbr0"
+  type        = string
+  default     = "vmbr0"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key to install in the VMs"
+  type        = string
+}

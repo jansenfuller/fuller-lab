ffcff03225ffcc4bc3b3b25e6b93a4d4538f7096
Jansen Fuller <jansendfuller@mailfence.com>
2026-01-08 16:40:01 -0700

    Updating install script to allow for new clusters and agent/server options

--- k8s/k3s.sh
+++ k8s/k3s.sh
@@ -15,7 +15,7 @@ mv .bashrc .bashrc_original
 # Run install for master nodes
 if [ $1 == "server" ]; then
     # Install K3S
-    curl -sfL https://get.k3s.io | sh -s - --tls-san load_balancer_ip_or_hostname
+    curl -sfL https://get.k3s.io | K3S_URL=$2 K3S_TOKEN=$3 sh -s - server --tls-san load_balancer_ip_or_hostname
 
     # Download and set new .bashrc
     wget -O .bashrc https://raw.githubusercontent.com/jansenfuller/fuller-lab/refs/heads/master/k8s/dev/serverrc.sh
@@ -27,15 +27,32 @@ if [ $1 == "server" ]; then
     sudo apt-get update
     sudo apt-get install helm
 
+    # Make sure url and token are properly set
+    echo "export K3S_URL=$1" >> .bashrc
+    echo "export K3S_TOKEN=$2" >> .bashrc
+
 # Run install for worker nodes
-else
+elif [ $1 == "agent" ]; then
     # Install K3S Agent
-    curl -sfL https://get.k3s.io | K3S_URL=$1 K3S_TOKEN=$2 sh -s - agent
+    curl -sfL https://get.k3s.io | K3S_URL=$2 K3S_TOKEN=$3 sh -s - agent
     wget -O .bashrc https://raw.githubusercontent.com/jansenfuller/fuller-lab/refs/heads/master/k8s/dev/serverrc.sh
 
     # Make sure url and token are properly set
     echo "export K3S_URL=$1" >> .bashrc
     echo "export K3S_TOKEN=$2" >> .bashrc
+
+else 
+    curl -sfL https://get.k3s.io | K3S_TOKEN=$1 sh -s - server --cluster-init --tls-san load_balancer_ip_or_hostname
+
+    # Download and set new .bashrc
+    wget -O .bashrc https://raw.githubusercontent.com/jansenfuller/fuller-lab/refs/heads/master/k8s/dev/serverrc.sh
+
+    # Install Helm
+    sudo apt-get install curl gpg apt-transport-https --yes
+    curl -fsSL https://packages.buildkite.com/helm-linux/helm-debian/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+    echo "deb [signed-by=/usr/share/keyrings/helm.gpg] https://packages.buildkite.com/helm-linux/helm-debian/any/ any main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+    sudo apt-get update
+    sudo apt-get install helm
 fi
 
 source .bashrc

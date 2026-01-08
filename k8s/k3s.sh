74fe4fec44a3aaa9f48f8e0b7b9600d9558a7d85
Jansen Fuller <jansendfuller@mailfence.com>
2026-01-08 09:09:02 -0700

    Update install commands

--- k8s/k3s.sh
+++ k8s/k3s.sh
@@ -11,13 +11,13 @@ mv .bashrc .bashrc_original
 
 # Run install for master nodes
 if [ $1 == "server" ]; then
-        curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --flannel-backend --disable=traefik" sh -s -
+        curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--flannel-backend --disable=traefik" sh -s - server --tls-san load_balancer_ip_or_hostname
 
         wget -O .bashrc https://raw.githubusercontent.com/jansenfuller/fuller-lab/refs/heads/master/k8s/dev/serverrc.sh
 
 # Run install for worker nodes
 else
-        curl -sfL https://get.k3s.io | K3S_URL=$1 K3S_TOKEN=$2 sh -s -
+        curl -sfL https://get.k3s.io | K3S_URL=$1 K3S_TOKEN=$2 sh -s - agent
         wget -O .bashrc https://raw.githubusercontent.com/jansenfuller/fuller-lab/refs/heads/master/k8s/dev/serverrc.sh
 
         echo "export K3S_URL=$1" >> .bashrc

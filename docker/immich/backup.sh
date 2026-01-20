da0753e2d21bcfb0b76391d4d18217cc548ef9da
Jansen Fuller <jansendfuller@mailfence.com>
2026-01-20 10:01:44 -0700

    Update backup

--- docker/immich/backup.sh
+++ docker/immich/backup.sh
@@ -1,6 +1,6 @@
 # Backup postgres installation
 
-cd /mnt/storage/postgres
+cd /mnt/storage/databases
 
 docker exec -t immich_postgres pg_dumpall -c -U postgres > ./immich_dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
